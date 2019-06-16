c89f4b135f2c545e6581424aa8ed5479fd24da25
Rafael Varago <rafael.varago@egym.de>
2019-06-16 19:57:16 +0200

    Introduce type class functor

---
+++ include/kitten/functor.h
@@ -0,0 +1,11 @@
+#ifndef RVARAGO_KITTEN_FUNCTOR_H
+#define RVARAGO_KITTEN_FUNCTOR_H
+
+namespace rvarago::kitten {
+
+    template<template<typename ...> typename F>
+    struct functor;
+
+}
+
+#endif
