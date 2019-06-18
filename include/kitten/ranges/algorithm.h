63913e0d63f741502d5d040b49f2168b47b545f2
Rafael Varago <rafael.varago@egym.de>
2019-06-18 12:59:04 +0200

    Add a range-based version of std::copy
    Meantime we don't have standard ranges yet.

---
+++ include/kitten/ranges/algorithm.h
@@ -0,0 +1,16 @@
+#ifndef RVARAGO_KITTEN_ALGORITHM_H
+#define RVARAGO_KITTEN_ALGORITHM_H
+
+#include <algorithm>
+
+namespace rvarago::kitten::ranges {
+
+    // TODO: Use standard ranges
+    template <typename Range, typename OutIterator>
+    decltype(auto) copy(Range&& range, OutIterator out) {
+        return std::copy(std::cbegin(range), std::cend(range), out);
+    }
+
+}
+
+#endif
