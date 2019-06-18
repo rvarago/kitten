2a655e8fa8882bb64bd841729779cec92b876ee1
Rafael Varago <rafael.varago@egym.de>
2019-06-18 13:03:01 +0200

    feature: Add support for new adapter: std::deque, std::list, std::vector

---
+++ include/kitten/detail/traits.h
@@ -0,0 +1,28 @@
+#ifndef RVARAGO_KITTEN_SEQUENCE_CONTAINER_H
+#define RVARAGO_KITTEN_SEQUENCE_CONTAINER_H
+
+#include <deque>
+#include <list>
+#include <vector>
+
+namespace rvarago::kitten::detail::traits {
+
+    // TODO: Use standard concepts
+    template <typename Container>
+    struct is_sequence_container final : std::false_type {};
+
+    template <typename T>
+    struct is_sequence_container<std::deque<T>> final : std::true_type {};
+
+    template <typename T>
+    struct is_sequence_container<std::list<T>> final : std::true_type {};
+
+    template <typename T>
+    struct is_sequence_container<std::vector<T>> final : std::true_type {};
+
+    template <typename Container>
+    using enable_if_sequence_container = typename std::enable_if_t<traits::is_sequence_container<Container>::value>;
+
+}
+
+#endif
---
+++ include/kitten/instances/sequence_container.h
@@ -0,0 +1,37 @@
+#ifndef RVARAGO_KITTEN_VECTOR_H
+#define RVARAGO_KITTEN_VECTOR_H
+
+#include "kitten/detail/traits.h"
+#include "kitten/functor.h"
+#include "kitten/monad.h"
+#include "kitten/ranges/algorithm.h"
+
+namespace rvarago::kitten {
+
+    template <template <typename...> typename SequenceContainer>
+    struct monad<SequenceContainer> {
+
+        template <typename UnaryFunction, typename A, typename... Rest, typename = detail::traits::enable_if_sequence_container<SequenceContainer<A, Rest...>>>
+        static auto bind(SequenceContainer<A, Rest...> const& input, UnaryFunction f) -> decltype(f(std::declval<A>())) {
+            auto mapped_sequence = decltype(f(std::declval<A>())){};
+            for (auto const& el : input) {
+                ranges::copy(f(el), std::back_inserter(mapped_sequence));
+            }
+            return mapped_sequence;
+        }
+
+    };
+
+    template <template <typename...> typename SequenceContainer>
+    struct functor<SequenceContainer> {
+
+        template <typename UnaryFunction, typename A, typename... Rest, typename = detail::traits::enable_if_sequence_container<SequenceContainer<A, Rest...>>>
+        static auto fmap(SequenceContainer<A, Rest...> const& input, UnaryFunction f) -> SequenceContainer<decltype(f(std::declval<A>()))> {
+            return monad<SequenceContainer>::bind(input, [&f](auto const& v) { return SequenceContainer{f(v)}; });
+        }
+
+    };
+
+}
+
+#endif
