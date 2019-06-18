2029ae26680a143ea3baf103d9f3d401cfcd1b13
Rafael Varago <rafael.varago@egym.de>
2019-06-18 12:56:06 +0200

    feature: Add free function fmap for functor instances

--- include/kitten/functor.h
+++ include/kitten/functor.h
@@ -3,9 +3,39 @@
 
 namespace rvarago::kitten {
 
-    template<template<typename ...> typename F>
+    /**
+     * A functor is an abstraction that allows to be mapped over.
+     *
+     * Given a functor fa: F[A] and a function f: A -> B
+     *  It uses f to map over over fa and then return a new functor fb: F[B].
+     *
+     * Laws:
+     *
+     * - Identity: fmap (f . g)  ==  fmap f . fmap g
+     * - Composition: fmap id = id
+     */
+    template <template <typename ...> typename F, typename Enable = void>
     struct functor;
 
+    /**
+     * Forwards to the functor implementation.
+     *
+     * @param input a functor fa: F[A]
+     * @param f a function A -> that maps over the value wrapped inside fa to yield a value b: B
+     * @return a new functor fb: F[B] resulting from applying f over the wrapped value inside fa
+     */
+    template <typename UnaryFunction, template <typename ...> typename F, typename A, typename... Rest>
+    decltype(auto) fmap(F<A, Rest...> const& input, UnaryFunction f) {
+        return functor<F>::fmap(input, f);
+    }
+
+    /**
+     * Infix version of fmap.
+     */
+    template <typename UnaryFunction, template <typename ...> typename F, typename A, typename... Rest>
+    decltype(auto) operator|(F<A, Rest...> const& input, UnaryFunction f) {
+        return fmap(input, f);
+    }
 }
 
 #endif
