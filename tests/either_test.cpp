72583e453389ae6e2893ae040c6f936fcbb55436
Rafael Varago <rafael.varago@egym.de>
2019-06-19 16:36:23 +0200

    tests: Check expected mapped types via static_assert

--- tests/either_test.cpp
+++ tests/either_test.cpp
@@ -3,7 +3,10 @@
 #include <string>
 #include <kitten/instances/either.h>
 
+#include "utils.h"
+
 using namespace rvarago::kitten;
+using test::utils::is_same_after_decaying;
 
 namespace {
 
@@ -16,6 +19,8 @@ namespace {
         auto const error = types::either<int, error_t>{error_t{-1}};
         auto const mapped_error = error | [](auto v){ return std::to_string(v * 10); };
 
+        static_assert(is_same_after_decaying<types::either<std::string, error_t>, decltype(mapped_error)>);
+
         EXPECT_TRUE(std::holds_alternative<error_t>(mapped_error));
         EXPECT_EQ(-1, std::get<error_t>(mapped_error).code);
     }
@@ -23,6 +28,8 @@ namespace {
     TEST(either, fmap_should_returnMapped_when_notEmpty) {
         auto const value_one = types::either<int, error_t>{1};
         auto const mapped_value = value_one | [](auto v){ return std::to_string(v * 10); };
+
+        static_assert(is_same_after_decaying<types::either<std::string, error_t>, decltype(mapped_value)>);
 
         EXPECT_TRUE(std::holds_alternative<std::string>(mapped_value));
         EXPECT_EQ("10", std::get<std::string>(mapped_value));
@@ -32,6 +39,8 @@ namespace {
         auto const error = types::either<int, error_t>{error_t{-1}};
         auto const mapped_error = error >> [](auto v){ return types::either<std::string, error_t>{std::to_string(v * 10)}; };
 
+        static_assert(is_same_after_decaying<types::either<std::string, error_t>, decltype(mapped_error)>);
+
         EXPECT_TRUE(std::holds_alternative<error_t>(mapped_error));
         EXPECT_EQ(-1, std::get<error_t>(mapped_error).code);
     }
@@ -39,6 +48,8 @@ namespace {
     TEST(either, bind_should_returnMapped_when_notEmpty) {
         auto const value_one = types::either<int, error_t>{1};
         auto const mapped_value = value_one >> [](auto v){ return types::either<std::string, error_t>{std::to_string(v * 10)}; };
+
+        static_assert(is_same_after_decaying<types::either<std::string, error_t>, decltype(mapped_value)>);
 
         EXPECT_TRUE(std::holds_alternative<std::string>(mapped_value));
         EXPECT_EQ("10", std::get<std::string>(mapped_value));
--- tests/optional_test.cpp
+++ tests/optional_test.cpp
@@ -3,7 +3,10 @@
 #include <string>
 #include <kitten/instances/optional.h>
 
+#include "utils.h"
+
 using namespace rvarago::kitten;
+using test::utils::is_same_after_decaying;
 
 namespace {
 
@@ -11,12 +14,16 @@ namespace {
         auto const none = std::optional<int>{};
         auto const mapped_none = none | [](auto v){ return std::to_string(v * 10); };
 
+        static_assert(is_same_after_decaying<std::optional<std::string>, decltype(mapped_none)>);
+
         EXPECT_TRUE(!mapped_none.has_value());
     }
 
     TEST(optional, fmap_should_returnMapped_when_notEmpty) {
         auto const some_one = std::optional<int>{1};
         auto const mapped_some = some_one | [](auto v){ return std::to_string(v * 10); };
+
+        static_assert(is_same_after_decaying<std::optional<std::string>, decltype(mapped_some)>);
 
         EXPECT_TRUE(mapped_some.has_value());
         EXPECT_EQ("10", mapped_some.value());
@@ -26,12 +33,16 @@ namespace {
         auto const none = std::optional<int>{};
         auto const mapped_none = none >> [](auto v){ return std::optional{std::to_string(v * 10)}; };
 
+        static_assert(is_same_after_decaying<std::optional<std::string>, decltype(mapped_none)>);
+
         EXPECT_TRUE(!mapped_none.has_value());
     }
 
     TEST(optional, bind_should_returnMapped_when_notEmpty) {
         auto const some_one = std::optional<int>{1};
         auto const mapped_some = some_one >> [](auto v){ return std::optional{std::to_string(v * 10)}; };
+
+        static_assert(is_same_after_decaying<std::optional<std::string>, decltype(mapped_some)>);
 
         EXPECT_TRUE(mapped_some.has_value());
         EXPECT_EQ("10", mapped_some.value());
