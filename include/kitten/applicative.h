b97ba5b87e7e7da3e9aa8e810f0eea56ef8e2412
Rafael Varago <rafael.varago@egym.de>
2019-06-26 16:58:02 +0200

    feature: Add new structure: Applicative
    That allows for mapping for functions that take several arguments,
    rather than only one as is the case for functors.
    
    Now we can map over a n-ary function:
    * combine(X<A>, X<B>, (A, B) -> C): X<C>

---
+++ include/kitten/applicative.h
@@ -0,0 +1,51 @@
+#ifndef RVARAGO_KITTEN_APPLICATIVE_H
+#define RVARAGO_KITTEN_APPLICATIVE_H
+
+#include <functional>
+#include <tuple>
+
+namespace rvarago::kitten {
+
+    /**
+     * An applicative is an abstraction that allows the combination of two wrapped values.
+     *
+     * Given two applicatives apa1: AP[A] and  apa2: AP[A] and a binary function f: A -> A -> B
+     *  It uses f to map over apa1 and apa2 and then return a new applicative apb: AP[B].
+     *
+     */
+    template <template <typename ...> typename AP, typename = void>
+    struct applicative;
+
+    /**
+     * Forwards to the applicative implementation.
+     *
+     * @param first an applicative apa1: AP[A]
+     * @param second an applicative apa2: AP[A]
+     * @param f a function A -> A -> B that maps over the values wrapped inside apa1 and apa2 to yield a value b: B
+     * @return a new applicative apb: AP[B] resulting from applying f over the wrapped value inside apa1 and apa2
+     */
+    template <typename BinaryFunction, template <typename ...> typename AP, typename A, typename B, typename... Rest>
+    constexpr decltype(auto) combine(AP<A, Rest...> const& first, AP<B, Rest...> const& second, BinaryFunction f) {
+        return applicative<AP>::combine(first, second, f);
+    }
+
+
+    /**
+     * Infix version of combine. Since operator+ expects two arguments, we had to wrap the applicatives in a tuple.
+     */
+    template <typename BinaryFunction, template <typename ...> typename AP, typename A, typename B, typename... Rest>
+    constexpr decltype(auto) operator+(std::tuple<AP<A, Rest...>, AP<B, Rest...>> const& input, BinaryFunction f) {
+        return combine(std::get<0>(input), std::get<1>(input), f);
+    }
+
+    /**
+     * Infix version of combine that receives unwrapped applicatives and uses + as a binary function.
+     */
+    template <template <typename ...> typename AP, typename A, typename B, typename... Rest>
+    constexpr decltype(auto) operator+(AP<A, Rest...> const& first, AP<B, Rest...> const& second) {
+        return combine(first, second, std::plus{});
+    }
+
+}
+
+#endif
--- include/kitten/kitten.h
+++ include/kitten/kitten.h
@@ -1,6 +1,7 @@
 #ifndef RVARAGO_KITTEN_KITTEN_H
 #define RVARAGO_KITTEN_KITTEN_H
 
+#include "kitten/applicative.h"
 #include "kitten/functor.h"
 #include "kitten/monad.h"
 #include "kitten/multifunctor.h"
