c8498f525aa2616e6a1afcbb97eb7fb9f733caf5
Rafael Varago <rafael.varago@egym.de>
2019-07-14 18:56:47 +0200

    cleanup: Move detail/traits.h content to sequence_container.h
    Since it's only used there.

--- include/kitten/instances/sequence_container.h
+++ include/kitten/instances/sequence_container.h
@@ -1,7 +1,11 @@
 #ifndef RVARAGO_KITTEN_SEQUENCE_CONTAINER_H
 #define RVARAGO_KITTEN_SEQUENCE_CONTAINER_H
 
-#include "kitten/detail/traits.h"
+#include <deque>
+#include <list>
+#include <type_traits>
+#include <vector>
+
 #include "kitten/applicative.h"
 #include "kitten/functor.h"
 #include "kitten/monad.h"
@@ -9,10 +13,30 @@
 
 namespace rvarago::kitten {
 
+    // TODO: Use standard concepts
+    namespace meta {
+
+        template <template <typename ...> typename Container>
+        struct is_sequence_container : std::false_type{};
+
+        template <>
+        struct is_sequence_container<std::deque> : std::true_type{};
+
+        template <>
+        struct is_sequence_container<std::list> : std::true_type{};
+
+        template <>
+        struct is_sequence_container<std::vector> : std::true_type{};
+
+        template <template <typename ...> typename Container>
+        using enable_if_sequence_container = typename std::enable_if_t<is_sequence_container<Container>::value>;
+
+    }
+
     template <template <typename...> typename SequenceContainer>
     struct monad<SequenceContainer> {
 
-        template <typename UnaryFunction, typename A, typename... Rest, typename = detail::traits::enable_if_sequence_container<SequenceContainer>>
+        template <typename UnaryFunction, typename A, typename... Rest, typename = meta::enable_if_sequence_container<SequenceContainer>>
         static constexpr auto bind(SequenceContainer<A, Rest...> const& input, UnaryFunction f) -> decltype(f(std::declval<A>())) {
             auto mapped_sequence = decltype(f(std::declval<A>())){};
             for (auto const& el : input) {
@@ -26,7 +50,7 @@ namespace rvarago::kitten {
     template <template <typename...> typename SequenceContainer>
     struct applicative<SequenceContainer> {
 
-        template <typename BinaryFunction, typename A, typename B, typename... Rest, typename = detail::traits::enable_if_sequence_container<SequenceContainer>>
+        template <typename BinaryFunction, typename A, typename B, typename... Rest, typename = meta::enable_if_sequence_container<SequenceContainer>>
         static constexpr auto combine(SequenceContainer<A, Rest...> const &first, SequenceContainer<B, Rest...> const& second, BinaryFunction f) -> SequenceContainer<decltype(f(std::declval<A>(), std::declval<B>()))> {
             using ValueT = decltype(f(std::declval<A>(), std::declval<B>()));
             auto combined_sequence = SequenceContainer<ValueT>{};
@@ -43,7 +67,7 @@ namespace rvarago::kitten {
     template <template <typename...> typename SequenceContainer>
     struct functor<SequenceContainer> {
 
-        template <typename UnaryFunction, typename A, typename... Rest, typename = detail::traits::enable_if_sequence_container<SequenceContainer>>
+        template <typename UnaryFunction, typename A, typename... Rest, typename = meta::enable_if_sequence_container<SequenceContainer>>
         static constexpr auto map(SequenceContainer<A, Rest...> const& input, UnaryFunction f) -> SequenceContainer<decltype(f(std::declval<A>()))> {
             return monad<SequenceContainer>::bind(input, [&f](auto const& v) { return SequenceContainer{f(v)}; });
         }
