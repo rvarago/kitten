46f2b88b7dcfb66fcb954220b5756d29264a2e3e
Rafael Varago <varago.rafael@gmail.com>
2019-11-14 15:51:47 +0100

    refactor: Implement generic applicative's combine and functor's fmap
    Since both can be implemented in terms of a monad instance and reused
    by most of the different applicatives and functors supported. Unless
    they demand some specific customization, e.g. for performance reasons.
    
    But for most cases, it simulates an automatic deriving fairly well.
    
    NOTE: Because of minor type-level mismatches, types::either is not
    using the generic implementation for now, but it shall do that in
    the future.

---
+++ include/kitten/detail/deriving/from_monad/derive_functor.h
@@ -0,0 +1,19 @@
+#ifndef RVARAGO_KITTEN_DERIVE_FUNCTOR_H
+#define RVARAGO_KITTEN_DERIVE_FUNCTOR_H
+
+#include "kitten/functor.h"
+#include "kitten/monad.h"
+
+namespace rvarago::kitten::detail::deriving {
+
+    template <typename UnaryFunction,template <typename ...> typename M, typename A, typename... Rest>
+    constexpr decltype(auto) fmap(M<A, Rest...> const& input, UnaryFunction f) {
+        using MonadT = monad<M>;
+        return MonadT::bind(input, [&f](auto const& value) {
+            return MonadT::wrap(f(value));
+        });
+    }
+
+}
+
+#endif
