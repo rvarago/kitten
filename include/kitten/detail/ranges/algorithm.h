5d4fccb96303247f9e491f8cda51b8ce01aae917
Rafael Varago <rafael.varago@egym.de>
2019-11-22 21:19:35 +0100

    refactor: Define and use detail::ranges::for_each
    Rather than for-loop.

--- include/kitten/detail/ranges/algorithm.h
+++ include/kitten/detail/ranges/algorithm.h
@@ -11,6 +11,11 @@ constexpr decltype(auto) copy(Range &&range, OutIterator out) {
     return std::copy(std::cbegin(range), std::cend(range), out);
 }
 
+template <typename Range, typename UnaryFunction>
+constexpr decltype(auto) for_each(Range &&range, UnaryFunction f) {
+    return std::for_each(std::cbegin(range), std::cend(range), f);
+}
+
 }
 
 #endif
